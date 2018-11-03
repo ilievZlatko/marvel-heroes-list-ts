4b0573688adf14cb60ac3440ce9d4307764a3cbb
Zlatko Iliev <iliev.zlatko@gmail.com>
2018-11-03 18:11:51 +0200

    finish the app

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from '../containers/Home';
 import Favourites from '../containers/Favourites';
+import Details from '../containers/Details';
 
 class Routes extends React.Component<any, any> {
     public render () {
@@ -10,6 +11,7 @@ class Routes extends React.Component<any, any> {
             <Switch>
                 <Route path="/" exact={true} component={Home} />
                 <Route path="/favourites" component={Favourites} {...this.props} />
+                <Route path="/details" component={Details} {...this.props} />
             </Switch> 
         );
     }
