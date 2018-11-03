169eb72b9e9f5b277b6211df8887dababdf62cb9
Zlatko Iliev <iliev.zlatko@gmail.com>
2018-11-03 19:42:26 +0200

    fix request bug

--- src/utils/marvelVariables.js
+++ src/utils/marvelVariables.js
@@ -1,3 +1,3 @@
 export const PUBLIC_KEY = 'fc2b330f668987538dc61e4d2a8211fb';
 export const PRIVATE_KEY = '9fb6ed54fc13c3fe4eef218b89896047d655e36f';
-export const BASE_URL = 'http://gateway.marvel.com/v1/public/';
+export const BASE_URL = 'https://gateway.marvel.com/v1/public/';
